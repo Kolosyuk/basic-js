1f2112137d206a74f53645bc9434e0a75a24f6ea
Kolosyuk <crplutt@gmail.com>
2020-09-23 23:50:18 +0300

    feat: now we can transform arrays!

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('ERROR');
+  let newArr = [...arr];
+  let res;
+  for (let i = 0; i < arr.length; i++) {
+
+    if (newArr[i] === '--discard-next' && i === newArr.length - 1) {
+      newArr[i] = 'discard';
+      continue
+    }
+    if (newArr[i] === '--discard-next' && i !== newArr.length - 1) {
+      newArr[i] = 'discard';
+      newArr[i + 1] = 'discard';
+      continue;
+    }
+
+    if (newArr[i] === '--discard-prev' && i === 0) {
+      newArr[i] = 'discard';
+      continue;
+    }
+
+    if (newArr[i] === '--discard-prev' && i !== 0) {
+      newArr[i - 1] = 'discard';
+      newArr[i] = 'discard';
+      continue;
+    }
+
+    if (newArr[i] === '--double-next' && i === newArr.length - 1) {
+      newArr[i] = 'discard';
+      continue;
+    }
+
+    if (newArr[i] === '--double-next' && i !== newArr.length - 1) {
+      newArr[i] = arr[i + 1];
+      continue;
+    }
+
+    if (newArr[i] === '--double-prev' && i === 0) {
+      newArr[i] = 'discard';
+      continue;
+    }
+    if (newArr[i] === '--double-prev' && i !== 0) {
+      newArr[i] = newArr[i - 1];
+      continue;
+    }
+  }
+  res = newArr.filter(item => item !== 'discard');
+  return res
 };
