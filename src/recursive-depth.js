147c1b723cf9640e9e3290fde32202c508a6f3e8
Kolosyuk <crplutt@gmail.com>
2020-09-28 10:27:26 +0300

    feat: implement recursive-depth function

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,28 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  constructor() {
-    this.counter = {
-      value: 1,
-      tempCounter: 1
-    };
-  }
 
-  calculateDepth(arr) {
-    if (!Array.isArray(arr) && this.counter.tempCounter === 0) {
-      throw new Error('не массив')
-    } else {
-      for (let i = 0; i < arr.length; i++) {
-        if (!Array.isArray(arr[i])) {
-          this.counter.value = this.counter.value < this.counter.tempCounter ? this.counter.tempCounter : this.counter.value;
-          this.counter.tempCounter = 1;
-          continue
-        } else {
-          ++this.counter.tempCounter;
-          this.calculateDepth(arr[i]);
-        }
-      }
-    }
-    return this.counter.value
+  calculateDepth(arr, counter = 1) {
+    return counter = arr.length !== 0 ? Math.max(...arr.map((item) => {
+      return item = Array.isArray(item) ? this.calculateDepth(item, counter + 1) : counter
+    })) : counter;
   }
 };
+
